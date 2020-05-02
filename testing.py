8a080c4b3c38986aa8e374ebbeb612f3d7d7e0ca
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2020-05-02 19:18:33 +0800

    edit testing

--- testing.py
+++ testing.py
@@ -1,15 +1,18 @@
-import requests
-import time
-n=1
-while(1):
-    try:
-        r = requests.get('http://127.0.0.1')
-        print(r.text[:20],n)
-        # r = requests.get('http://127.0.0.1/draw')
-        # print(r.text[:10],n)
-        # r = requests.get('http://127.0.0.1/alldata')
-        # print(r.text[:10],n)
-        n=n+1
-        time.sleep(2)
-    except:
-        None
+#coding:utf-8
+# iTaiwan中央行政機關室內公共區域免費無線上網熱點查詢服務
+import json 
+import re
+f = open("C:\\Users\\user\\Desktop\\wifi_info.json", "r",encoding="utf-8")
+# print(f.read()[:100]) 
+format=json.loads(f.read())
+# print(len(format))
+for i in range(len(format)):
+    r=re.findall(r"^[台臺]中.*",format[i]['NAME'])
+    if r!=[]:
+        print(format[i])
+        # print(format[i]['NAME'],format[i]['LATITUDE'],format[i]['LONGITUDE'])
+
+
+
+# r=re.findall(r"^[台臺]中[^嘉義]","臺中榮總嘉義分院門診部門診大樓2F候診區")
+# print(r)
